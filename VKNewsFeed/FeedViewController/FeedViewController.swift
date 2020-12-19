5886318271044479708a309e316402a53d491772
Sawka01 <8744934@gmail.com>
2020-12-20 02:54:23 +0500

    Implementing Parsing Logic

--- VKNewsFeed/FeedViewController/FeedViewController.swift
+++ VKNewsFeed/FeedViewController/FeedViewController.swift
@@ -10,13 +10,19 @@ import UIKit
 
 class FeedViewController: UIViewController {
 
-    private let networkService = NetworkService()
+    private var fetcher: DataFetcher = NetworkDataFetcher(networking: NetworkService())
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         view.backgroundColor = #colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1)
-//        networkService.getFeed()
+        fetcher.getFeed { (feedResponse) in
+            guard let feedResponse = feedResponse else { return }
+            feedResponse.items.map { (feedItem) in
+                print(feedItem.date)
+            }
+        }
+
     }
 
 }
---
+++ VKNewsFeed/Services/Models/FeedResponse.swift
@@ -0,0 +1,32 @@
+//
+//  FeedResponse.swift
+//  VKNewsFeed
+//
+//  Created by Macbook on 20.12.2020.
+//  Copyright © 2020 Sawka uz. All rights reserved.
+//
+
+import Foundation
+
+struct FeedResponseWrapped: Decodable {
+    let response: FeedResponse
+}
+
+struct FeedResponse: Decodable {
+    var items: [FeedItem]
+}
+
+struct FeedItem: Decodable {
+    let sourceId: Int
+    let postId: Int
+    let text: String?
+    let date: Double
+    let comments: CountableItem?
+    let likes: CountableItem?
+    let reposts: CountableItem?
+    let views: CountableItem?
+}
+
+struct CountableItem: Decodable {
+    let count: Int
+}
