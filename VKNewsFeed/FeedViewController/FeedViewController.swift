28ea2cef08e7fb985f47906c9c5f65351ea5dba1
Sawka01 <8744934@gmail.com>
2020-12-20 01:43:39 +0500

    Fetch News Feed JSON

--- VKNewsFeed/FeedViewController/FeedViewController.swift
+++ VKNewsFeed/FeedViewController/FeedViewController.swift
@@ -16,7 +16,7 @@ class FeedViewController: UIViewController {
         super.viewDidLoad()
 
         view.backgroundColor = #colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1)
-        networkService.getFeed()
+//        networkService.getFeed()
     }
 
 }
