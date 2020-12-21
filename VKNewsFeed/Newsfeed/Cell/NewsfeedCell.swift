df51ac86b994dcb5e2ac9250e88bf44ee7aaa12a
Sawka01 <8744934@gmail.com>
2020-12-21 05:46:26 +0500

    Parsing Data Into Cells

--- VKNewsFeed/Newsfeed/Cell/NewsfeedCell.swift
+++ VKNewsFeed/Newsfeed/Cell/NewsfeedCell.swift
@@ -9,6 +9,17 @@
 import Foundation
 import UIKit
 
+protocol FeedCellViewModel {
+    var iconUrlString: String { get }
+    var name: String { get }
+    var date: String { get }
+    var text: String? { get }
+    var likes: String? { get }
+    var comments: String? { get }
+    var shares: String? { get }
+    var views: String? { get }
+}
+
 class NewsfeedCell: UITableViewCell {
 
     static let reuseId = "NewsfeedCell"
@@ -21,11 +32,21 @@ class NewsfeedCell: UITableViewCell {
     
     @IBOutlet weak var likesLabel: UILabel!
     @IBOutlet weak var commentsLabel: UILabel!
-    @IBOutlet weak var sharesLabel: UIImageView!
+    @IBOutlet weak var sharesLabel: UILabel!
     @IBOutlet weak var viewsLabel: UILabel!
 
     override func awakeFromNib() {
         super.awakeFromNib()
         
+    }
+
+    func set(viewModel: FeedCellViewModel) {
+        nameLabel.text = viewModel.name
+        dateLabel.text = viewModel.date
+        postLabel.text = viewModel.text
+        likesLabel.text = viewModel.likes
+        commentsLabel.text = viewModel.comments
+        sharesLabel.text = viewModel.shares
+        viewsLabel.text = viewModel.views
     }
 }
